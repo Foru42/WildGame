7251d30b205c361260b626410509c0bc8867abb7
Foru42 <josufory@gmail.com>
2025-01-09 10:03:13 +0100

    Lehen Comita

---
+++ Assets/MenuManager.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public void playButton()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
